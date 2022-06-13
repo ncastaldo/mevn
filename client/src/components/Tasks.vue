437431ea94d73356525c2d4a62c79dbed6a75357
Nicola Castaldo <nicolacastaldo123@gmail.com>
2022-06-14 00:02:10 +0200

    client

---
+++ client/src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import { getTasks, createTask } from '../services/TaskService'
+
+const tasks = ref([])
+
+tasks.value = await getTasks();
+
+</script>
+
+<template>
+  <h1>Tasks</h1>
+  <div>
+    <div v-for="(task, i) in tasks" :key="i">
+      <h5>{{task}}</h5>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
---
+++ client/src/services/TaskService.js
@@ -0,0 +1,19 @@
+const baseApi = "/api";
+
+const getTasks = async function () {
+  const response = await fetch(`${baseApi}/tasks`);
+  console.log(response);
+  return await response.json();
+};
+
+const createTask = async function (task) {
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(task),
+  };
+  const response = await fetch(`${baseApi}/tasks`, options);
+  return await response.json();
+};
+
+export { getTasks, createTask };
